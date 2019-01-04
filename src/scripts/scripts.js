0440b900c3d8b79c302e3fe3a695f961c1c56bfa
Артём Изосимов <1996loko@rambler.ru>
2019-01-04 04:36:20 +0300

    site production

--- src/scripts/scripts.js
+++
@@ -1,3 +0,0 @@
-const githubSection = document.getElementById('github-section');
-const skillsSection = document.getElementById('skills-section');
-const worksSection = document.getElementById('works-section');
